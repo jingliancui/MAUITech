09e86b7972e217211a0f13cd39988523ea4cf867
Eric <ericcui@qq.com>
2023-12-21 17:57:07 +0800

    add compiled binding support

---
+++ src/Obfuscar/ReleaseSample/SampleApp/SampleApp/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleApp
+{
+    public class MainPageViewModel: ObservableObject
+    {
+        public MainPageViewModel()
+        {
+            OpenNewWinCommand = new RelayCommand(OpenNewWin);
+            OpenNewWinBtnText = "Open a New Window";
+        }
+
+        public ICommand OpenNewWinCommand { get; set; }
+        public void OpenNewWin()
+        {
+            var secondPage = new Window(new SecondPage());
+            Application.Current.OpenWindow(secondPage);
+        }
+
+        private string openNewWinBtnText;
+        public string OpenNewWinBtnText
+        {
+            get => openNewWinBtnText;
+            set => SetProperty(ref openNewWinBtnText, value);
+        }
+    }
+}
