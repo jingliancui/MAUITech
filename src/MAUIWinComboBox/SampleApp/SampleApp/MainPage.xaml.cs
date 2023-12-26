527a44ef944aa345c261c6f204b5d1f18aa4897e
Eric <ericcui@qq.com>
2023-12-26 13:19:46 +0800

    add win combobox sample

--- src/MAUIWinComboBox/SampleApp/SampleApp/MainPage.xaml.cs
+++ src/MAUIWinComboBox/SampleApp/SampleApp/MainPage.xaml.cs
@@ -1,24 +1,20 @@
-﻿namespace SampleApp
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace SampleApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+
+            Loaded += MainPage_Loaded;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void MainPage_Loaded(object? sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            var winCombo = picker.Handler!.PlatformView as ComboBox;
+            winCombo!.IsEditable = true;
         }
     }
 
